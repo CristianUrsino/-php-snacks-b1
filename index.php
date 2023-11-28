a6501ba4f995867965b6802bb159d9c4997925f4
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-28 14:47:13 +0100

    added first snack

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+$partite = [
+    [
+        'squadraCasa' => 'inter',
+        'squadraOspite' => 'jv',
+        'puntiCasa'=> 3,
+        'puntiOspite'=> 2,
+    ],
+    [
+        'squadraCasa' => 'Napoli',
+        'squadraOspite' => 'Milan',
+        'puntiCasa'=> 0,
+        'puntiOspite'=> 4,
+    ],
+    [
+        'squadraCasa' => 'Pippo',
+        'squadraOspite' => 'Pluto',
+        'puntiCasa'=> 2,
+        'puntiOspite'=> 2,
+    ]
+]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +29,16 @@
     <title>Snacks</title>
 </head>
 <body>
-    
+    <main>
+        <section>
+            <h1>SNACK 1</h1>
+            <h3>CALENDARIO: </h3>
+            <?php foreach($partite as $partita){
+                echo $partita['squadraCasa'] . ' - ' . $partita['squadraOspite'] . ' | ' . $partita['puntiCasa'] . ' - ' . $partita['puntiOspite'] . '<br>';
+            } ?>
+        </section>
+        <section>
+        </section>
+    </main>
 </body>
 </html>

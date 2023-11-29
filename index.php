febf8b4dfd982212c3286f00024c5bceca30330a
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-29 11:29:25 +0100

    added second snack

--- index.php
+++ index.php
@@ -18,7 +18,18 @@ $partite = [
         'puntiCasa'=> 2,
         'puntiOspite'=> 2,
     ]
-]
+];
+
+if(isset($_GET['email']) && isset($_GET['name']) && isset($_GET['age']) && isset($_GET['check'])){
+    $email = $_GET['email'];
+    $name = $_GET['name'];
+    $age = $_GET['age'];
+    $check = $_GET['check'];
+    $flagInputControl = false;
+    if(strlen($name) > 3 && str_contains($email,'.') && str_contains($email, '@') && is_numeric($age) && $check=='on'){
+        $flagInputControl = true;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +49,38 @@ $partite = [
             } ?>
         </section>
         <section>
+            <h1>SNACK 2</h1>
+            <?php if(isset($_GET['email']) && isset($_GET['name']) && isset($_GET['age']) && isset($_GET['check'])){
+                if($flagInputControl){
+                    echo "<div>Accesso riuscito</div>";
+                }else{
+                    echo "<div class='red'>Accesso NON riuscito</div>";
+                }
+            }else{ ?>
+            <form action="index.php" method="GET">
+                <div>
+                    <label for="email">email</label>
+                    <input type="text" name="email">
+                </div>
+                <div>
+                    <label for="name">name</label>
+                    <input type="text" name="name">
+                </div>
+                <div>
+                    <label for="age">age</label>
+                    <input type="number" name="age">
+                </div>
+                <div>
+                    <input type="checkbox" name="check">
+                    <label name="check">
+                        Default checkbox
+                    </label>
+                </div>
+                <div>
+                    <input type="submit">
+                </div>
+            </form>
+            <?php } ?>
         </section>
     </main>
 </body>
